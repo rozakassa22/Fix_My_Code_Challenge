781f67cff5b31f962f38e335dad9b4dc9c94f4d6
RozaKassa <rosepinkyyy33.com>
2024-02-25 12:55:19 -0800

    Fix_My_Code_Challenge

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -22,9 +22,8 @@ if (isNaN(size) || size <= 0) {
 }
 
 for (let i = 0; i < size; i++) {
-    let row = "";
     for (let j = 0; j < size; j++) {
-        row += "#";
+        process.stdout.write("#");
     }
-    console.log(row);
+    process.stdout.write("\n");
 }
