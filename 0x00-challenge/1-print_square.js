98f1b87dccb5d35c538cbb3807c422a4cab44441
RozaKassa <rosepinkyyy33.com>
2024-02-25 12:49:23 -0800

    Fix_My_Code_Challenge

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,12 +6,11 @@
     of the program.
 */
 
-
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1)
+    process.exit(1);
 }
 
 const size = parseInt(process.argv[2]);
@@ -23,8 +22,9 @@ if (isNaN(size) || size <= 0) {
 }
 
 for (let i = 0; i < size; i++) {
+    let row = "";
     for (let j = 0; j < size; j++) {
-        process.stdout.write("#");
+        row += "#";
     }
-    process.stdout.write("\n");
+    console.log(row);
 }
