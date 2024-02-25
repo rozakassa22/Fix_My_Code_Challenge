e26cf3b6792e9a876568f3d98de1ee0caac467c7
RozaKassa <rosepinkyyy33.com>
2024-02-25 13:06:13 -0800

    Fix_My_Code_Challenge

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,14 +6,18 @@
     of the program.
 */
 
-if (process.argv.length <= 2) {
-    process.stderr.write("Missing argument\n");
+if (process.argv.length !== 3) {
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
     process.exit(1);
 }
 
 const size = parseInt(process.argv[2]);
+
+if (isNaN(size) || size <= 0) {
+    process.stderr.write("Invalid size. Please provide a positive integer.\n");
+    process.exit(1);
+}
 
 for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
